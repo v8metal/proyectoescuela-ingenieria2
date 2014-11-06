fc5f4e393fda628cc3a457fd86cbb56a14a30542
assantangelo@gmail.com <assantangelo@gmail.com@1d8b9ec4-ec45-be44-0cf2-8f4c58ca8b63>
2014-11-06 00:54:47 +0000

    test datepicker para entrevistas admin

--- trunk/SistemaEscuela/WebContent/js/entrevista.js
+++ trunk/SistemaEscuela/WebContent/js/entrevista.js
@@ -1,17 +1,14 @@
-$(function () {
-    
-	var mes = $('#mesbase').val();
-		
-	$('#mes option').each(function () {
-		
-		var val = $(this).val();
-		
-		if (val < mes){
-			
-			$(this).remove();
-			
-		}
-	 
-	});
-	 
-});
+		 	$(function () {
+			    
+		 		$("#datepicker").datepicker({
+		 		   dateFormat: "dd/mm/yy",
+		 		   //defaultDate: '05/11/2014',
+		 		   showOn: "both",
+		 		   buttonText: "Pick a date",
+		 		   //yearRange: '2014:2014',
+		 		   //changeMonth: true, para poder selecionar el mes desde un dropdown		 		   
+		 		   beforeShowDay: $.datepicker.noWeekends,
+		 		   minDate:'0',
+		 	       maxDate: '+3m',   //add this
+		 		});		 					
+		});
